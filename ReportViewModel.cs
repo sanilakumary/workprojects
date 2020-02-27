db61d3d05a9ad5a3da65bd97616bc64c6614e9c2
sanilakumary <48491134+sanilakumary@users.noreply.github.com>
2020-02-28 00:13:21 +0100

    Update ReportViewModel.cs

--- ReportViewModel.cs
+++ ReportViewModel.cs
@@ -8,11 +8,11 @@ namespace ExpenseCalculatorPresentation.ViewModel
 {
     public class ReportViewModel
     {
-        //public ReportType ReportType { get; set; }
+        
         public List<ReportType> ReportTypes { get; set; }
         public List<ExpenseType> ExpenseTypes { get; set; }
         public List<ExpenseData> ExpenseData { get; set; }
-        //public IEnumerable<ExpenseData> ExpenseData { get; set; }
+        
 
     }
 }
